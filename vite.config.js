5d9a79cb23966ccd87af0c3ff5f049034e19ac7f
Clark <clark_aloysius_deleon@dlsl.edu.ph>
2025-10-04 08:41:05 +0800

    vite.config change again

--- vite.config.js
+++ vite.config.js
@@ -30,22 +30,27 @@ export default defineConfig({
           }
         ]
       },
-     workbox: {
-  globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-  maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/api\./,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'api-cache',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 60 * 60 * 24
-        }
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\./,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24
+              }
+            }
+          }
+        ]
       }
+    }), react()],
+      build: {
+    rollupOptions: {
+      external: ['bootstrap']
     }
-  ]
-}
-    }),react()],
+  }
 })
